eae8b543b5395a7e171334a815ee31f6c23d6849
Abhay007 <abhay007official@gmail.com>
2025-05-29 14:59:56 +0530

    `Added cors`

--- Program.cs
+++ Program.cs
@@ -8,10 +8,39 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Add services
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "Google";
+})
+.AddCookie("Cookies")
+.AddGoogle("Google", options =>
+{
+    options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+    options.CallbackPath = "/signin-google";
+});
+
+builder.Services.AddAuthorization();
 builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddDbContext<NeonDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins",
+        builder =>
+        {
+            builder.WithOrigins(
+                "http://localhost:3000",        // Localhost (dev)
+                "https://moco-frontend.vercel.app" // Vercel live URL
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials(); // needed for cookies/session
+        });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +51,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseMiddleware<ApiKeyMiddleware>();
+
+app.UseCors("AllowSpecificOrigins");
 
 app.UseHttpsRedirection();
 
